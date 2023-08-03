b38d0fb4eb347d8263aadb39518dd3ae67d17417
Natalia Savchenko <n.savchenko@gmarkg.com>
2023-08-04 00:15:07 +0300

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = n.toString().split('');
+    let newStr = '';
+
+    for (let i = 0; i < str.length; i++) {
+        if (!Number.isNaN(+str[i])) {
+            newStr += str[i];
+        }
+    }
+
+    return newStr.split('').reverse().join('');
 }
